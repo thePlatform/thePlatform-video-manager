44f211dd10831fade9314c84c01f15a839b3cd43
Matan Bareket <matan.bareket@lt-mbareket-mac.corp.theplatform.com>
2014-10-28 18:59:04 -0400

    Handle update from 1.2.2/1.3.0 to 1.3.1
    The mix up in the versions had a bug, so we need to clean up the
    settings.

--- thePlatform-helper.php
+++ thePlatform-helper.php
@@ -325,6 +325,14 @@ function theplatform_check_plugin_update() {
 	update_option( TP_PREFERENCES_OPTIONS_KEY,  $newPreferences );
 	update_option( TP_ACCOUNT_OPTIONS_KEY,      array_merge( TP_ACCOUNT_OPTIONS_DEFAULTS(),     get_option( TP_ACCOUNT_OPTIONS_KEY,     array() ) ) );
 	update_option( TP_UPLOAD_OPTIONS_KEY,       array_merge( TP_UPLOAD_FIELDS_DEFAULTS(),       get_option( TP_UPLOAD_OPTIONS_KEY,	    array() ) ) );  
+	
+	// We had a messy update with 1.2.2/1.3.0, let's clean up	
+	if ( ( $oldVersion['major'] == '1' && $oldVersion['minor'] == '2' && $oldVersion['patch'] == '2' ) ||
+		 ( $oldVersion['major'] == '1' && $oldVersion['minor'] == '3' && $oldVersion['patch'] == '0' ) ) {
+		$basicMetadataFields = get_option( TP_UPLOAD_OPTIONS_KEY, array() );
+		$customMetadataFields = get_option( TP_METADATA_OPTIONS_KEY, array() );
+		update_option ( TP_UPLOAD_OPTIONS_KEY, array_diff_assoc( $basicMetadataFields, $customMetadataFields ) );
+	}
 
 	// Move account settings from preferences (1.2.0)	
 	if ( ( $oldVersion['major'] == '1' && $oldVersion['minor'] <  '2' ) && 
