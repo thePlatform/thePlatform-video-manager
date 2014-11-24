2cbca2f153e306f3f820d6f8034b7a32ac59f9ac
Matan Bareket <matan.bareket@lt-mbareket-mac.corp.theplatform.com>
2014-11-24 11:27:19 -0500

    Set default embed type to release
    one URL is still not enabled by default, stay on release

--- thePlatform-constants.php
+++ thePlatform-constants.php
@@ -64,7 +64,7 @@ function TP_PREFERENCES_OPTIONS_DEFAULTS() {
 		'default_height'       => intval( ( $GLOBALS['content_width'] / 16 ) * 9 ),
 		'player_embed_type'    => 'embed',
 		'embed_hook'           => 'tinymce',
-		'media_embed_type'     => 'pid'
+		'media_embed_type'     => 'release'
 	);
 }
 
